de976133e1089af979f58a956483affc783befc2
isuru <isurudevj@gmail.com>
2023-10-15 14:22:06 +0530

    Stomp wip.

---
+++ autotrader-app/src/test/java/io/github/isurudevj/cseautotrader/AutoTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.isurudevj.cseautotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
